ad435804458562f2d781823c7ae728f21d8fcd3c
Sherine <83099016+sherine-reda@users.noreply.github.com>
2023-04-02 17:44:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,15 +29,15 @@ function App() {
   }
  
   const routers = createBrowserRouter([
-    {path:'e-commerce-App',element:<Layout setUserData={setUserData} userData={userData}/> , children:[
+    {path:'',element:<Layout setUserData={setUserData} userData={userData}/> , children:[
       {index:true,element:<ProtectedRoute><Home /></ProtectedRoute>},
-      {path:'e-commerce-App/cart',element:<ProtectedRoute><Cart/></ProtectedRoute>},
-      {path:'e-commerce-App/products',element:<ProtectedRoute><Products/></ProtectedRoute>},
-      {path:'e-commerce-App/productDetails/:id',element:<ProtectedRoute><ProductDetails/></ProtectedRoute>},
-      {path:'e-commerce-App/categories',element:<ProtectedRoute><Categories/></ProtectedRoute>},
-      {path:'e-commerce-App/brands',element:<ProtectedRoute><Brands/></ProtectedRoute>},
-      {path:'e-commerce-App/login',element:<Login saveUserData={saveUserData}/>},
-      {path:'e-commerce-App/register',element:<Register/>},
+      {path:'cart',element:<ProtectedRoute><Cart/></ProtectedRoute>},
+      {path:'products',element:<ProtectedRoute><Products/></ProtectedRoute>},
+      {path:'productDetails/:id',element:<ProtectedRoute><ProductDetails/></ProtectedRoute>},
+      {path:'categories',element:<ProtectedRoute><Categories/></ProtectedRoute>},
+      {path:'brands',element:<ProtectedRoute><Brands/></ProtectedRoute>},
+      {path:'login',element:<Login saveUserData={saveUserData}/>},
+      {path:'register',element:<Register/>},
       {path:'*',element:<Notfound/>},
 
     ]}
